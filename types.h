534730337d20e40b9252c017c1c5051d63f012d8
ytatichno <ytatichno@gmail.com>
2024-01-08 15:24:38 +0300

    fix some types

--- types.h
+++ types.h
@@ -10,6 +10,19 @@
 #include <fcntl.h> 
 #endif
 #include <bits/types.h>
+#ifdef JOSBUILD
+
+// #include </home/ytatichno/os/prac/inc/assert.h>
+
+extern "C" {
+    #define restrict __restrict
+    // #include <stdnoreturn.h>
+    #define _Noreturn [[noreturn]]
+    // #include </home/ytatichno/os/prac/inc/string.h>  // for strnlen
+    // #include </home/ytatichno/os/prac/inc/stdio.h>  // for snprintf
+    #include </home/ytatichno/os/prac/inc/lib.h>  // for snprintf
+}
+#endif
 
 
 typedef unsigned int u4;
@@ -32,8 +45,8 @@ typedef double f8;
 
 #define LOINT64(I8) (u4)(I8 &0xFFFFFFFF)
 #define HIINT64(I8) (u4)(I8 >> 32)
-#define getu4(p) (u4)( (u4)((p)[0])<<24 & 0xFF000000 | (u4)((p)[1])<<16 & 0x00FF0000 | (u4)((p)[2])<<8 & 0x0000FF00| (u4)((p)[3]) & 0x000000FF)
-#define getu2(p) (u2)((p)[0]<< 8 & 0x0000FF00 |(p)[1])
+#define getu4(p) (u4)( ((u4)((p)[0])<<24 & 0xFF000000) | ((u4)((p)[1])<<16 & 0x00FF0000) | ((u4)((p)[2])<<8 & 0x0000FF00)| ((u4)((p)[3]) & 0x000000FF))
+#define getu2(p) (u2)(((p)[0]<< 8 & 0x0000FF00) |((p)[1]))
 
 #define MAKEI8(i4high, i4low) (i8) (((i8)i4high) << 32 | (i8)i4low)
 
